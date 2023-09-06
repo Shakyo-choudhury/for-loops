70714e2dca1cb07cbd248a5ab7d0f9ef62dec310
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-09-06 21:58:56 +0530

    Add files via upload

---
+++ tutorial18.py
@@ -0,0 +1,15 @@
+# for loops
+# what are loops?
+# counter controlled loop - for loop
+# syntax
+'''
+for iterator_var in sequence:
+    statements(s)
+'''
+n = 4
+for i in range(0, n):
+    print(i)
+# list
+l = ["minecraft", "fortnite", "fifa"]
+for i in l:
+    print(i)
